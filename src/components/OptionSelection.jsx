16f54a409eaec7d458cfc34cc555d5f5b811a667
Sami <51018077+khalosamilo@users.noreply.github.com>
2023-02-11 12:39:50 +0100

    Update OptionSelection.jsx

--- src/components/OptionSelection.jsx
+++ src/components/OptionSelection.jsx
@@ -23,7 +23,7 @@ export default function OptionSelection({ arrayItems, selectOption }) {
       <div className="grid-pic">
         <div onClick={() => window.location.href = "https://your-example-pics.netlify.app.example"}>              
           <h3>Picture Generator</h3>
-          <pCcreate a random generated picture by using a good defined prompt.</p>
+          <p>create a random generated picture by using a good defined prompt.</p>
         </div>
       </div>
     </>
